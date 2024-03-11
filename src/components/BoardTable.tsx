a40a7f6f4d573bb583c8c8604ff5e10a2ede14c1
지은 <jisilver.kim@gmail.com>
2024-03-11 18:42:25 +0900

    테이블 구현

---
+++ src/components/BoardTable.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export interface IBoardTableProps {
+    //children?: React.ReactNode,
+}
+
+const BoardTable = ({}: IBoardTableProps) => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+
+BoardTable.defaultProps = {};
+
+export default BoardTable;
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,4 +1,6 @@
 import useFetch from "../hooks/useFetch.ts";
+import {DataGrid, GridColDef} from '@mui/x-data-grid';
+
 
 export interface IBoardProps {
     //children?: React.ReactNode,
@@ -7,11 +9,35 @@ export interface IBoardProps {
 const Board = ({}: IBoardProps) => {
 
     const {data} = useFetch('/api/board')
-    console.log(data)
+
+
+    const rows = [
+        { id: 1, title: 'Hello', view: 0, author: 'user1', createdAt: '2024-03-11'},
+        { id: 2, title: 'Hello', view: 0, author: 'user1', createdAt: '2024-03-11'},
+        { id: 3, title: 'Hello', view: 0, author: 'user1', createdAt: '2024-03-11'},
+    ];
+
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 150 },
+        { field: 'title', headerName: '제목', width: 150 },
+        { field: 'view', headerName: '조회수', width: 150 },
+        { field: 'author', headerName: '작성자', width: 150 },
+        { field: 'createdAt', headerName: '작성일', width: 150 },
+    ];
 
     return (
-        <div>
-            게시판
+        <div className={"mt-5 w-full"}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+            />
         </div>
     );
 };
